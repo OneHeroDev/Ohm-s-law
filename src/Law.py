c92e41d16f89cf14319eb93401639aab755611ad
rahulranjan937 <ranjan.rahul937@gmail.com>
2020-07-04 23:50:35 +0530

    new changes

---
+++ src/Law.py
@@ -0,0 +1,13 @@
+# Ohm's Law
+
+# Calculating Voltage
+def voltage(v,i,r):
+    return v == i * r
+
+# Calculating Resistance
+def resistance (v,i,r):
+    return r == v/r
+
+# Calculating Current
+def current(v,i,r):
+    return i == v/r
