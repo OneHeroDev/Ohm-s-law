4a1cdd605e88ddeeaf83987f3ea992304a66b80c
rahulranjan937 <ranjan.rahul937@gmail.com>
2020-07-05 00:19:51 +0530

    adds lines

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
 # Ohm's Law
+
+from src import Law
+
+ask = True
+
+while ask:
+    print("--------------------------")
+    print("--------Ohm's law---------")
+    print("1. Calculate Voltage")
+    print("2. Calculate Resistance")
+    print("3. Calculate Current")
+    print("--------------------------")
+    ask = input("Choose An Option (1-3): ")
+    if (ask == 1):
+        print ("-- Calculating Voltage --")
+        i = float(input("   Enter the current: "))
+        r = float(input("   Enter the resistence: "))
+        break
