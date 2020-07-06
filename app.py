35e9413d4b86c5c06e2b613be1b7a5c6a6b12fd1
rahulranjan937 <ranjan.rahul937@gmail.com>
2020-07-06 23:52:29 +0530

    adds lines

--- app.py
+++ app.py
@@ -15,20 +15,20 @@ while ask:
     ask = input("\nChoose An Option (1-3): ")
     if ask == "1":
         print ("-- Calculating Voltage --")
-        i = float(input("   Enter the current(In Amps (A)): "))
-        r = float(input("   Enter the resistence(In Ohms(Ω)): "))
+        i = float(input("Enter the current(In Amps (A)): "))
+        r = float(input("Enter the resistence(In Ohms(Ω)): "))
         result = Law.voltage(i, r)
         print(i, "*", r, "=", result)
     elif ask == "2":
         print ("-- Calculating Resistance --")
-        v = float(input("    Enter the Voltage(In Volts (V)): "))
-        i = float(input("    Enter the current(In Amps (A)): "))
+        v = float(input("Enter the Voltage(In Volts (V)): "))
+        i = float(input("Enter the current(In Amps (A)): "))
         result = Law.resistance(v, i)
         print(v, "/", i, "=", result)
     elif ask == "3":
         print ("-- Calculating Current --")
-        v = float(input("    Enter the Voltage(In Volts (V)): "))
-        r = float(input("    Enter the resistence(In Ohms(Ω)): "))
+        v = float(input("Enter the Voltage(In Volts (V)): "))
+        r = float(input("Enter the resistence(In Ohms(Ω)): "))
         result = Law.current(v, r)
         print(v, "/", r, "=", result)
     else:
